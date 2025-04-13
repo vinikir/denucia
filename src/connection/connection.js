945e5b26d670e3e3f1142919b61e4dcd826b111e
vinikir <vinicius@clubedodev.com.br>
2025-04-13 04:24:52 -0300

    ajustes

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -2,10 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
    
-    //baseURL:"http://18.222.164.74/",
-    //baseURL:"https://urvu65w6he.execute-api.us-east-2.amazonaws.com/",
-    baseURL:"http://127.0.0.1:3300/",
-  
+   
+    //baseURL:"http://127.0.0.1:3300/",
+    baseURL:"https://denuncia-backend.onrender.com",
     timeout: 30000,
     headers: {
         'Content-Type':'application/json; charset=utf-8',
